390b944fdb9b63aa7f1abe37aeb57d880426b679
Narongsak <54994033+sscarry2@users.noreply.github.com>
2021-01-05 20:43:56 +0700

    personal-site

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+};
+
+showMenu('nav-toggle', 'nav-menu');
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  // Active Link
+  navLink.forEach((nl) => nl.classList.remove('active'));
+  this.classList.add('active');
+
+  // Remove menu mobile
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show');
+}
+
+navLink.forEach((nl) => nl.addEventListener('click', linkAction));
